eba03bdce1f8c8872a2d48aa0924c99ee67e179a
genemills <genemills@gmail.com>
2013-10-22 09:38:11 -0400

    Bitmaker Fizzbuzz

---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+var = (1..100)
+var = (1..100).each do |i|
+    
+    if i % 5 == 0 and i % 3 == 0
+        puts "BitMaker"
+    elsif i % 5 == 0
+        puts "Maker"
+    elsif i % 3 == 0
+        puts "Bit"
+    else
+        puts i
+    end
+   end
