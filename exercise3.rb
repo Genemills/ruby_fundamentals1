679580e11010dacd9b922eb6426190e1804e618a
genemills <genemills@gmail.com>
2013-10-22 09:25:59 -0400

    adding age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,13 @@
 puts "What is your name?"
+
 name = gets.chomp
-puts "Your name is #{name}"
+
+puts "Hi #{name}!"
+
+puts "How old are you #{name}?"
+
+age = gets.chomp.to_i # convert string to integer
+
+year = 2013-age
+
+puts "You were born in #{year}"
